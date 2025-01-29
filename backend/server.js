c62dda2aad5231cb26c20630a847830c07f99202
Luís <luissteam44@gmail.com>
2025-01-29 12:59:44 -0300

    day 1 progress

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,44 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from './models/product.model.js'
 
 dotenv.config();
 
 const app = express();
 
-app.get("/" , (req,res) => {
-    res.send("Server is ready");
+app.use(express.json());
+
+app.post("/api/products" , async (req,res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in create product: ", error.message);
+        res.status(500).json({success: false, message: "Server Error"}); 
+    }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success:true, message: "Product deleted"})
+    } catch (error) {
+        res.status(400).json({success: false, message: "Product not found"})
+    }
 })
-
-
 
 app.listen(5000, ()=>{
     connectDB();
     console.log('Server started at http://localhost:5000')
 })
-
-
-
