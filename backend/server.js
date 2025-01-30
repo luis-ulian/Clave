e43ae9f177284d5d9b2c897553d1dc7538b0cbc8
Luís <luissteam44@gmail.com>
2025-01-30 10:50:29 -0300

    day 2 progress

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,15 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/products" , async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(201).json({success: true, data: products})
+    } catch (error) {
+        res.status(500).json({success: false, message: "Server error"});
+    }
+})
+
 app.post("/api/products" , async (req,res) => {
     const product = req.body;
 
@@ -27,18 +36,35 @@ app.post("/api/products" , async (req,res) => {
     }
 });
 
+app.patch("/api/products/:id", async (req, res) => {
+    const {id} = req.params
+
+    const product = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "Product not found"})
+    }
+
+    try {
+        const updateProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(201).json({success: true, data: updateProduct});
+    } catch (error) {
+        res.status(500).json({success: false, message: "Server error"});
+    }
+})
+
 app.delete("/api/products/:id", async (req, res) => {
     const {id} = req.params;
 
     try {
         await Product.findByIdAndDelete(id);
-        res.status(200).json({ success:true, message: "Product deleted"})
+        res.status(200).json({ success:true, message: "Product deleted"});
     } catch (error) {
-        res.status(400).json({success: false, message: "Product not found"})
+        res.status(400).json({success: false, message: "Product not found"});
     }
 })
 
 app.listen(5000, ()=>{
     connectDB();
-    console.log('Server started at http://localhost:5000')
+    console.log('Server started at http://localhost:5000');
 })
