11061f5e9cad65ca5f6ed319612e156d6b445012
Luís <luissteam44@gmail.com>
2025-02-11 12:18:20 -0300

    login and logout functions on backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,10 +49,44 @@ export const signup = async (req,res) => {
     }
 };
 
-export const login = (req,res) => {
-    res.send("");
-};
-export const logout = (req,res) => {
-    res.send("");
+export const login = async (req,res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        });
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error"}); 
+    }
 };
 
+export const logout = (req,res) => {
+    try {
+        res.cookie("jwt", "", {maxAge:0});
+        res.status(200).json({message: "Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+};
+
+export const updateProfile = async (req,res) => {
+    
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { login, logout, signup } from "../controllers/auth.controller.js"
+import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js"
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.put("/update-profile", updateProfile);
 
 export default router
