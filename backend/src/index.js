a6a4491d9b21107c900805539f6e86bf3ed54f91
Luís <luissteam44@gmail.com>
2025-02-11 08:50:20 -0300

    token generator and signup endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js";
 import {connectDB} from "./lib/db.js"
+
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
