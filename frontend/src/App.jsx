3fe2bd75b6bf05d0d0565fcc2a22e7e861614a3e
Luís Ulian <luissteam44@gmail.com>
2025-02-19 13:30:53 -0300

    some ajusts because im using my pc again and frontend zustand call to authenticate on refresh page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
-
 import { Routes, Route } from 'react-router-dom'
 
 import HomePage from './pages/HomePage.jsx'
@@ -8,8 +7,10 @@ import SignUpPage from './pages/SignUpPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
+import { useAuthStore } from './store/useAuthStore.js'
 
 const App = () => {
+  const {useAuth} = useAuthStore();
   return (
     <div>
       <Navbar/>
