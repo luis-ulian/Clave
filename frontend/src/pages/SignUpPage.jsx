7e5e037dceeffab025d31df85a1d3c81269c24c9
Luís Ulian <luissteam44@gmail.com>
2025-02-25 13:19:15 -0300

    sign up page with logo and input areas

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import {useAuthStore} from '../store/useAuthStore'
-import { MessageSquare } from 'lucide-react';
 import "../styles/SignUpPage.css"
+import {Eye, EyeOff, Lock} from "lucide-react"
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -14,20 +14,93 @@ const SignUpPage = () => {
   const {signup, isSigningUp} = useAuthStore();
 
   const validateForm = () => {
-
+    
   }
+
   const handleSubmit = (e) => {
     e.preventDefault();
   }
   return (
     <div className='signup'>
-      {/* left side */}
-      <div className='leftColumn'>
-        <div className='logo'>
-            <MessageSquare className='size-6 text-primary'/>
+      {/* LEFT SIDE */}
+      <div className='leftSide'>
+        <div className='leftSideController'>  
+          {/* LOGO */ }
+          <div className='logoDiv'>
+            <div className="logoController group">
+              <img src="pngegg.png" alt="atizap2-logo" className='logo group-hover:bg-primary/20 transition-colors'/>
+              <h1 className='text-2xl font-bold mt-2'>Criar conta</h1>
+              <p className='text-base-content/60'>Comece agora a usar o Conversinhas!</p>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit} className='space-y-6'>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Nome completo</span>
+              </label>
+              <div className='inputController'>
+                <div className="inputImg">
+                  <img src="user.png" alt="profileImg" className='img'/>
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="João Nascimento"
+                  value={formData.fullName}
+                  onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <div className="inputController">
+                <div className="inputImg">
+                  <img src="email-line-icon-png.png" alt="mail img" className="img" />
+                </div>
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-tex">Senha</span>
+              </label>
+              <div className="inputController">
+                <div className="inputImg">
+                  <Lock className ="img"></Lock>
+                </div>
+                <input 
+                  type= {showPassword ? "text" : "password"} 
+                  className='input input-bordered w-full pl-10 text-base-content/40'
+                  placeholder='*******'
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value})}
+                 />
+                 <button
+                  type='button'
+                  className='showPassBtn'
+                  onClick={() => setShowPassword(!showPassword)}>
+
+                  {showPassword ? (<EyeOff className='img'/>
+                  ) : (<Eye className='img'/>)}
+                  
+                 </button>
+              </div>
+            </div>
+
+            
+          </form>
         </div>
-        <h1 className='text-2xl font-bold mt-2'>Create Account</h1>
-        <p className='text-base-content/60'>Get started with your free account</p>
       </div>
     </div>
   )
